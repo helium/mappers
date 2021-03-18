b1dd2056ad95e8f332640545d159cc57e9d8ed67
Kent Williams <k3nt00@gmail.com>
2021-03-18 13:42:21 -0700

    move message parsing to specific data creates

--- lib/mappers/ingest/ingest.ex
+++ lib/mappers/ingest/ingest.ex
@@ -1,24 +1,20 @@
 defmodule Mappers.Ingest do
   alias Mappers.Uplinks
 
-  def ingest_uplink(%{"app_eui" => app_eui, "dev_eui" => dev_eui, "id" => device_id, "fcnt" => fcnt, "reported_at" => first_timestamp, "frequency" => frequency, "gps_accuracy" => gps_accuracy, "spreading" => spreading_factor}) do
+  def ingest_uplink(message) do
     # validate that lat/lng actually make sense
     # validate device location with hotspot locations
 
     # create new h3_res9 record if it doesn't exist
+    #H3.create(message)
     # create uplink record
-    attrs = %{}
-      |> Map.put(:app_eui, app_eui)
-      |> Map.put(:dev_eui, dev_eui)
-      |> Map.put(:device_id, device_id)
-      |> Map.put(:fcnt, fcnt)
-      |> Map.put(:first_timestamp, first_timestamp |> DateTime.from_unix!())
-      |> Map.put(:frequency, frequency)
-      |> Map.put(:gps_accuracy, gps_accuracy)
-      |> Map.put(:spreading_factor, spreading_factor)
-
-    Uplinks.create(attrs)
+    resp = Uplinks.create(message)
+    {_, uplink} = resp
+    IO.puts(uplink.id)
     # create uplinks_heard
+    #UplinksHeard.create(message)
     # create h3/uplink link
+    #Links.create(h3_res_id, uplink_id)
+    resp
   end
 end
--- lib/mappers/uplinks/uplinks.ex
+++ lib/mappers/uplinks/uplinks.ex
@@ -2,7 +2,16 @@ defmodule Mappers.Uplinks do
   alias Mappers.Repo
   alias Mappers.Uplinks.Uplink
 
-  def create(uplink) do
+  def create(message) do
+    uplink = %{}
+      |> Map.put(:app_eui, message["app_eui"])
+      |> Map.put(:dev_eui, message["dev_eui"])
+      |> Map.put(:device_id, message["id"])
+      |> Map.put(:fcnt, message["fcnt"])
+      |> Map.put(:first_timestamp, message["reported_at"] |> DateTime.from_unix!())
+      |> Map.put(:frequency, Enum.at(message["hotspots"], 0)["frequency"])
+      |> Map.put(:gps_accuracy, 2)
+      |> Map.put(:spreading_factor, Enum.at(message["hotspots"], 0)["spreading"])
 
     %Uplink{}
     |> Uplink.changeset(uplink)
