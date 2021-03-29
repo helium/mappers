49865ea16178cd8eabcae51b3d8d5d7f4f77dd75
Kent Williams <k3nt00@gmail.com>
2021-03-28 21:19:10 -0700

    first csv load attempt complete

--- lib/mix/tasks/load_mappers_csv.ex
+++ lib/mix/tasks/load_mappers_csv.ex
@@ -12,19 +12,17 @@ defmodule Mix.Tasks.LoadMappersCsv do
     IO.puts("Starting......")
 
     IO.puts("Loading CSVs.....")
-    hotspots = load_csv("~/code/mappers/mappers/hotspots_full.csv")
-    uplinks = load_csv("~/code/mappers/mappers/uplinks_full.csv")
+    hotspots = load_csv_file("~/code/mappers/mappers/hotspots_full.csv")
+    # uplinks = load_csv_stream("~/code/mappers/mappers/uplinks_full.csv")
+    uplinks_csv_streams = load_split_csv("~/code/mappers/mappers/split-uplinks")
 
-    IO.puts("hotspot rows: #{Enum.count(hotspots)}")
-    IO.puts("uplink rows: #{Enum.count(uplinks)}")
-
-    # procs = Enum.to_list(1..16)
-    procs = [1]
+    # IO.puts("hotspot rows: #{Enum.count(hotspots)}")
+    # IO.puts("uplink rows: #{Enum.count(uplinks)}")
 
     pid =
-      procs
-      |> Enum.map(fn proc_id ->
-        spawn(fn -> load_uplink(proc_id, uplinks, hotspots) end)
+      uplinks_csv_streams
+      |> Enum.map(fn uplinks ->
+        spawn(fn -> load_uplink(uplinks, hotspots) end)
       end)
 
     # Start monitoring `pid`
@@ -37,64 +35,106 @@ defmodule Mix.Tasks.LoadMappersCsv do
     end
   end
 
-  def load_uplink(num, uplinks, hotspots) do
+  def load_uplink(uplinks, hotspots) do
+
     Enum.each(uplinks, fn uplink ->
       uplink_id = Enum.at(uplink, 0)
-      IO.puts(num)
-      IO.puts(uplink_id)
+      # IO.puts(num)
+      # IO.puts(uplink_id)
 
       hotspot_index =
         Enum.find_index(hotspots, fn hotspot -> uplink_id == Enum.at(hotspot, 1) end)
 
-      hotspot = Enum.at(hotspots, hotspot_index)
-      hotspot_uplink_id = Enum.at(hotspot, 1)
+      # {time_micro, _} = :timer.tc(fn -> Enum.find_index(hotspots, fn hotspot -> uplink_id == Enum.at(hotspot, 1) end) end)
 
-      IO.puts(hotspot_uplink_id)
+      # IO.puts(time_micro/1000000)
 
-      if hotspot_uplink_id != nil do
+      if hotspot_index != nil do
+        hotspot = Enum.at(hotspots, hotspot_index)
+        hotspot_uplink_id = Enum.at(hotspot, 1)
 
-        decoded_map = %{
-          "payload" => %{
-            "latitude" => if Enum.at(uplink, 9) == "0" do 0.0 else String.to_float(Enum.at(uplink, 9)) end ,
-            "longitude" => if Enum.at(uplink, 10) == "0" do 0.0 else String.to_float(Enum.at(uplink, 10)) end
+        # IO.puts(hotspot_uplink_id)
+
+        if hotspot_uplink_id != nil do
+          decoded_map = %{
+            "payload" => %{
+              "latitude" =>
+                if Enum.at(uplink, 9) == "0" do
+                  0.0
+                else
+                  String.to_float(Enum.at(uplink, 9))
+                end,
+              "longitude" =>
+                if Enum.at(uplink, 10) == "0" do
+                  0.0
+                else
+                  String.to_float(Enum.at(uplink, 10))
+                end
+            }
           }
-        }
 
-        hotspots_map = %{
-          "id" => Enum.at(hotspot, 0),
-          "name" => Enum.at(hotspot, 2),
-          "lat" => if Enum.at(hotspot, 4) == "0" do 0.0 else String.to_float(Enum.at(hotspot, 4)) end,
-          "long" => if Enum.at(hotspot, 5) == "0" do 0.0 else String.to_float(Enum.at(hotspot, 5)) end,
-          "rssi" => Kernel.trunc(String.to_float(Enum.at(hotspot, 6))),
-          "snr" => String.to_float(Enum.at(hotspot, 7)),
-          "frequency" => 0,
-          "spreading" => Enum.at(hotspot, 8),
-          "reported_at" => (DateTime.to_unix(elem(DateTime.from_iso8601("#{Enum.at(hotspot, 9)}Z"), 1))*1000)
-        }
+          hotspots_map = %{
+            "id" => Enum.at(hotspot, 0),
+            "name" => Enum.at(hotspot, 2),
+            "lat" =>
+              if Enum.at(hotspot, 4) == "0" do
+                0.0
+              else
+                String.to_float(Enum.at(hotspot, 4))
+              end,
+            "long" =>
+              if Enum.at(hotspot, 5) == "0" do
+                0.0
+              else
+                String.to_float(Enum.at(hotspot, 5))
+              end,
+            "rssi" => Kernel.trunc(String.to_float(Enum.at(hotspot, 6))),
+            "snr" => String.to_float(Enum.at(hotspot, 7)),
+            "frequency" => 0,
+            "spreading" => Enum.at(hotspot, 8),
+            "reported_at" =>
+              DateTime.to_unix(elem(DateTime.from_iso8601("#{Enum.at(hotspot, 9)}Z"), 1)) * 1000
+          }
 
-        message =
-          %{}
-          |> Map.put("id", Enum.at(uplink, 1))
-          |> Map.put("app_eui", "0")
-          |> Map.put("dev_eui", Enum.at(uplink, 3))
-          |> Map.put("reported_at", (DateTime.to_unix(elem(DateTime.from_iso8601("#{Enum.at(uplink, 5)}Z"), 1)))*1000)
-          |> Map.put("fcnt", String.to_integer(Enum.at(uplink, 7)))
-          |> Map.put("decoded", decoded_map)
-          |> Map.put("hotspots", [hotspots_map])
-          |> Map.put("gps_accuracy", String.to_integer(Enum.at(uplink, 14)))
+          message =
+            %{}
+            |> Map.put("id", Enum.at(uplink, 1))
+            |> Map.put("app_eui", "0")
+            |> Map.put("dev_eui", Enum.at(uplink, 3))
+            |> Map.put(
+              "reported_at",
+              DateTime.to_unix(elem(DateTime.from_iso8601("#{Enum.at(uplink, 5)}Z"), 1)) * 1000
+            )
+            |> Map.put("fcnt", String.to_integer(Enum.at(uplink, 7)))
+            |> Map.put("decoded", decoded_map)
+            |> Map.put("hotspots", [hotspots_map])
+            |> Map.put("gps_accuracy", String.to_integer(Enum.at(uplink, 14)))
 
-        Ingest.ingest_uplink(message)
+          Ingest.ingest_uplink(message)
+        end
       end
     end)
   end
 
-  def load_csv(path) do
+  def load_split_csv(dir) do
+    {_, files} = File.ls(Path.expand(dir))
+
+    Enum.map(files, fn file ->
+      load_csv_file("#{Path.expand(dir)}/#{file}")
+    end)
+  end
+
+  def load_csv_stream(path) do
+    path
+    |> Path.expand(__DIR__)
+    |> File.stream!(read_ahead: 100_000)
+    |> CSV.parse_stream()
+  end
+
+  def load_csv_file(path) do
     path
     |> Path.expand(__DIR__)
     |> File.read!()
     |> CSV.parse_string()
-
-    # |> File.stream!()
-    # |> CSV.parse_stream()
   end
 end
