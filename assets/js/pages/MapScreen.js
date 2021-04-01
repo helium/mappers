b430d558a511d8ea9f08c09dbdf3bf6e756316d3
Kent Williams <k3nt00@gmail.com>
2021-04-01 16:24:35 -0700

    add barebones mapbox gl map

---
+++ assets/js/pages/MapScreen.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Map from "../components/Map"
+
+class MapScreen extends React.Component {
+  render() {
+    return (
+        <Map />
+    )
+  }
+}
+
+export default MapScreen;
