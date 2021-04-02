50ec29737f188519708b6e01a19a10a1152299a3
Kent Williams <k3nt00@gmail.com>
2021-04-02 13:05:48 -0700

    upgrade elixir version to 1.9 for heroku

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mappers.MixProject do
     [
       app: :mappers,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
