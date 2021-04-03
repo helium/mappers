7782ee8f72c4b39ec6d9a31327814e89e53d18f5
Kent Williams <k3nt00@gmail.com>
2021-04-03 12:58:52 -0700

    add current map style and h3 tileserver

--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -7,15 +7,18 @@ mapboxgl.accessToken = process.env.PUBLIC_MAPBOX_KEY;
 const Map = () => {
   const mapContainerRef = useRef(null);
 
-  const [lng, setLng] = useState(5);
-  const [lat, setLat] = useState(34);
-  const [zoom, setZoom] = useState(1.5);
+  const [lng, setLng] = useState(-122.21);
+  const [lat, setLat] = useState(37.58);
+  const [zoom, setZoom] = useState(10);
+
+  const [map, setMap] = useState(null);
+  const sourceId = 'public.h3_res9';
 
   // Initialize map when component mounts
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/petermain/ckmwdn50a1ebk17o3h5e6wwui',
       center: [lng, lat],
       zoom: zoom
     });
@@ -27,6 +30,36 @@ const Map = () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
+    });
+
+    map.on('load', () => {
+      map.addSource('h3-vector-db', {
+        type: 'vector',
+        url: `https://mappers-tileserver.herokuapp.com/${sourceId}.json`
+      });
+
+      map.addLayer({
+        id: sourceId,
+        type: 'fill',
+        source: 'h3-vector-db',
+        'source-layer': sourceId,
+        'paint': {
+          'fill-color': [
+            'interpolate',
+            ['linear'],
+            ['get', 'average_rssi'],
+            -120,
+            'rgba(38,251,202,0.1)',
+            -100,
+            'rgba(38,251,202,0.45)',
+            -80,
+            'rgba(38,251,202,0.8)'
+          ],
+          'fill-opacity': 0.9
+        }
+      });
+
+      setMap(map);
     });
 
     // Clean up on unmount
