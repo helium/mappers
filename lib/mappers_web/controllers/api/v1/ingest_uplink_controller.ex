8ca7537b14408e2cbe09fadf23193787c064c081
Kent Williams <k3nt00@gmail.com>
2021-04-05 11:56:46 -0700

    remove unnecessary alias

--- lib/mappers_web/controllers/api/v1/ingest_uplink_controller.ex
+++ lib/mappers_web/controllers/api/v1/ingest_uplink_controller.ex
@@ -2,7 +2,6 @@ defmodule MappersWeb.API.V1.IngestUplinkController do
   use MappersWeb, :controller
 
   alias Mappers.Ingest
-  alias Mappers.Uplinks.Uplink
 
   def create(conn, params) do
     resp = Ingest.ingest_uplink(params)
