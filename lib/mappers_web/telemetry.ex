6151b3318c31b8ca1a083e41b17d708d68a95ef9
Kent Williams <k3nt00@gmail.com>
2021-04-08 08:59:25 -0700

    updated prometheus metrics reporter to use https

--- lib/mappers_web/telemetry.ex
+++ lib/mappers_web/telemetry.ex
@@ -14,7 +14,7 @@ defmodule MappersWeb.Telemetry do
       {:telemetry_poller, measurements: periodic_measurements(), period: 10_000},
       # Add reporters as children of your supervision tree.
       # {Telemetry.Metrics.ConsoleReporter, metrics: metrics()}
-      {TelemetryMetricsPrometheus, [metrics: prometheus_periodic_measurements()]}
+      {TelemetryMetricsPrometheus, [metrics: prometheus_periodic_measurements(), protocol: :https, plug_cowboy_opts: [keyfile: "priv/cert/selfsigned_key.pem", certfile: "priv/cert/selfsigned.pem", otp_app: :mappers]]}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
