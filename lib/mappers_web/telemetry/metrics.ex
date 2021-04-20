f18999039a431625dce586fee6be3f0250ae10c2
Kent Williams <k3nt00@gmail.com>
2021-04-20 14:28:50 -0700

    add h3 prom metrics (#10)

--- lib/mappers_web/telemetry/metrics.ex
+++ lib/mappers_web/telemetry/metrics.ex
@@ -1,8 +1,13 @@
 defmodule MappersWeb.Telemetry.Metrics do
   require Logger
 
-  def handle_event([:ingest, :request], %{duration: dur}, metadata, _config) do
+  def handle_event_h3_res9_new([:ingest, :h3, :res9, :new], %{h3_res9_id: h3_res9_id}, metadata, _config) do
     # do some stuff like log a message or report metrics to a service like StatsD
-    Logger.info("Received [:ingest, :request] event. Request duration: #{dur}, Route: #{metadata.request_path}")
+    Logger.info("Received [:ingest, :h3, :res9, :new] event. New H3: #{h3_res9_id}")
+  end
+
+  def handle_event_h3_res9_existing([:ingest, :h3, :res9, :existing], %{h3_res9_id: h3_res9_id}, metadata, _config) do
+    # do some stuff like log a message or report metrics to a service like StatsD
+    Logger.info("Received [:ingest, :h3, :res9, :existing] event. Existing H3: #{h3_res9_id}")
   end
 end
