fc70bd4ba052d068e5104fcb1a69bba2234beb2d
Kent Williams <k3nt00@gmail.com>
2021-04-21 11:13:58 -0700

    Add Hex Click Action (#12)
    * add mouse enter/leave cursor on map
    
    * add on click popup for hex

--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -32,6 +32,33 @@ const Map = () => {
       setZoom(map.getZoom().toFixed(2));
     });
 
+    // Change the cursor to a pointer when the mouse is over a hexagon.
+    map.on('mouseenter', 'public.h3_res9', function () {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'public.h3_res9', function () {
+      map.getCanvas().style.cursor = '';
+    });
+
+    map.on('click', 'public.h3_res9', function (e) {
+      var coordinates = e.features[0].geometry.coordinates[0][0];
+      var description = e.features[0].properties.average_rssi;
+      
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+      
+      new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML('rssi: ' + description)
+      .addTo(map);
+    });
+
     map.on('load', () => {
       map.addSource('h3-vector-db', {
         type: 'vector',
