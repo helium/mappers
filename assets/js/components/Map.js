8314876f8b0744e9eceaf007071a5b1988c791f6
Kent Williams <k3nt00@gmail.com>
2021-04-23 12:03:03 -0700

    add socket to map and populate new h3 layer

--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import '../../css/app.css'
+import socket from "../socket"
+import geojson2h3 from 'geojson2h3';
 
 mapboxgl.accessToken = process.env.PUBLIC_MAPBOX_KEY;
 
@@ -44,22 +46,86 @@ const Map = () => {
 
     map.on('click', 'public.h3_res9', function (e) {
       var coordinates = e.features[0].geometry.coordinates[0][0];
-      var description = e.features[0].properties.average_rssi;
-      
+      var rssi = e.features[0].properties.average_rssi;
+
       // Ensure that if the map is zoomed out such that multiple
       // copies of the feature are visible, the popup appears
       // over the copy being pointed to.
       while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
       }
-      
+
       new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML('rssi: ' + description)
-      .addTo(map);
+        .setLngLat(coordinates)
+        .setHTML('rssi: ' + rssi)
+        .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over a hexagon.
+    map.on('mouseenter', 'new-h3', function () {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'new-h3', function () {
+      map.getCanvas().style.cursor = '';
+    });
+
+    map.on('click', 'new-h3', function (e) {
+      var coordinates = e.features[0].geometry.coordinates[0][0];
+      var rssi = e.features[0].properties.average_rssi;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML('rssi: ' + rssi)
+        .addTo(map);
     });
 
     map.on('load', () => {
+      let features = []
+
+      let channel = socket.channel("h3:new")
+      channel.on("new_h3", payload => {
+        features.push(geojson2h3.h3ToFeature(payload.body.h3_id, {'average_rssi': payload.body.average_rssi}))
+        const featureCollection =
+        {
+          "type": "FeatureCollection",
+          "features": features
+        }
+        // Update map data source
+        map.getSource('new-h3').setData(featureCollection)
+      })
+
+      channel.join()
+        .receive("ok", resp => { console.log("Joined successfully", resp) })
+        .receive("error", resp => { console.log("Unable to join", resp) })
+
+      map.addSource('new-h3', {
+        type: 'geojson', data:
+        {
+          "type": "FeatureCollection",
+          "features": []
+        }
+      });
+
+      map.addLayer({
+        id: 'new-h3',
+        type: 'fill',
+        source: 'new-h3',
+        'layout': {},
+        'paint': {
+          'fill-color': '#faf409',
+          'fill-opacity': 0.9
+        }
+      });
+
       map.addSource('h3-vector-db', {
         type: 'vector',
         url: `https://mappers-tileserver.herokuapp.com/${sourceId}.json`
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -54,10 +54,4 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 // Finally, connect to the socket:
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
-
 export default socket
