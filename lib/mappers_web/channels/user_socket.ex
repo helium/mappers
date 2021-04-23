ebd94d1885e4f1801d545d432dbf6eadad3871ce
Kent Williams <k3nt00@gmail.com>
2021-04-23 11:59:22 -0700

    add channel broadcast of new h3 ingested

--- lib/mappers_web/channels/user_socket.ex
+++ lib/mappers_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule MappersWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", MappersWeb.RoomChannel
+  channel "h3:*", MappersWeb.H3Channel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
