44accd8957eeba9b408987c96ac0e18ecb1fc52c
Kent Williams <k3nt00@gmail.com>
2021-06-03 10:15:37 -0700

    put MapGL in container div

--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -1,29 +1,31 @@
 import * as React from 'react';
-import {useState} from 'react';
-import {render} from 'react-dom';
+import { useState } from 'react';
+import { render } from 'react-dom';
 import MapGL from 'react-map-gl';
 
 const MAPBOX_TOKEN = process.env.PUBLIC_MAPBOX_KEY;
 
 function Map() {
-  const [viewport, setViewport] = useState({
-    latitude: 37.8,
-    longitude: -122.4,
-    zoom: 14,
-    bearing: 0,
-    pitch: 0
-  });
+    const [viewport, setViewport] = useState({
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+    });
 
-  return (
-    <MapGL
-      {...viewport}
-      width="100vw"
-      height="100vh"
-      mapStyle="mapbox://styles/petermain/ckmwdn50a1ebk17o3h5e6wwui"
-      onViewportChange={setViewport}
-      mapboxApiAccessToken={MAPBOX_TOKEN}
-    />
-  );
+    return (
+        <div className='map-container'>
+            <MapGL
+                {...viewport}
+                width="100vw"
+                height="100vh"
+                mapStyle="mapbox://styles/petermain/ckmwdn50a1ebk17o3h5e6wwui"
+                onViewportChange={setViewport}
+                mapboxApiAccessToken={MAPBOX_TOKEN}
+            />
+        </div>
+    );
 }
 
 export default Map;
