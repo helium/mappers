c5281422e7e71aadc1bbcb0aa79d184ff7522c77
Kent Williams <k3nt00@gmail.com>
2021-06-04 11:36:21 -0700

    Add Live Uplinks Channel  (#20)
    * add channel socket uplinks
    
    * clean up layers fields

--- assets/js/components/Layers.js
+++ assets/js/components/Layers.js
@@ -31,22 +31,52 @@ export const uplinkTileServerLayer = {
 
 export const uplinkHotspotsLineLayer = {
     id: 'uplinkHotspotsLineLayer',
-    'type': 'line',
-    'layout': {
+    type: 'line',
+    layout: {
         'line-join': 'round',
         'line-cap': 'round'
     },
-    'paint': {
+    paint: {
         'line-color': '#d8d51d',
         'line-width': 2
     }
 };
 
 export const uplinkHotspotsCircleLayer = {
-    'id': 'uplinkHotspotsCircleLayer',
-    'type': 'circle',
-    'layout': {},
-    'paint': {
+    id: 'uplinkHotspotsCircleLayer',
+    type: 'circle',
+    paint: {
         'circle-color': '#d8d51d',
+    }
+};
+
+export const uplinkChannelLayer = {
+    id: 'uplinkChannelLayer',
+    type: 'fill',
+    paint: {
+        'fill-color': [
+            'case',
+            ['boolean',
+                ['feature-state', 'selected'], true],
+            ['interpolate',
+                ['linear'],
+                ['get', 'avg_rssi'],
+                -120,
+                'rgba(38,251,202,0.1)',
+                -100,
+                'rgba(38,251,202,0.45)',
+                -80,
+                'rgba(38,251,202,0.8)']
+            ,
+            '#b67ffe'
+        ],
+        'fill-opacity': 0.9,
+        'fill-outline-color': [
+            'case',
+            ['boolean',
+                ['feature-state', 'selected'], true],
+            'rgba(38,251,202,0.45)',
+            '#FFFFFF'
+        ]
     }
 };
--- lib/mappers_web/controllers/api/v1/ingest_uplink_controller.ex
+++ lib/mappers_web/controllers/api/v1/ingest_uplink_controller.ex
@@ -5,7 +5,7 @@ defmodule MappersWeb.API.V1.IngestUplinkController do
 
   def create(conn, params) do
     resp = Ingest.ingest_uplink(params)
-    MappersWeb.Endpoint.broadcast!("h3:new", "new_h3", %{body: %{h3_id: resp.h3_res9_id, avg_rssi: resp.avg_rssi}})
+    MappersWeb.Endpoint.broadcast!("h3:new", "new_h3", %{body: %{id: resp.h3_res9_id, avg_rssi: resp.avg_rssi, avg_snr: resp.avg_snr}})
     conn |> json(resp.resp)
   end
 end
