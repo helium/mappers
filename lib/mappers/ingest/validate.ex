3c5c563d34c5833002df642827827361c8361fcf
Kent Williams <k3nt00@gmail.com>
2021-06-22 09:54:14 -0700

    add validation check for snr value

--- lib/mappers/ingest/validate.ex
+++ lib/mappers/ingest/validate.ex
@@ -6,6 +6,7 @@ defmodule Mappers.Ingest.Validate do
     hotspot_lat = Enum.at(message["hotspots"], 0)["lat"]
     hotspot_lng = Enum.at(message["hotspots"], 0)["long"]
     hotspot_rssi = Enum.at(message["hotspots"], 0)["rssi"]
+    hotspot_snr = Enum.at(message["hotspots"], 0)["snr"]
 
     if device_lat == 0.0 or device_lat < -90 or device_lat > 90 or device_lng == 0.0 or
          device_lng < -180 or device_lng > 180 do
@@ -20,10 +21,14 @@ defmodule Mappers.Ingest.Validate do
              500_000 do
           {:error, "Invalid Distance Between Device and Hotspot"}
         else
-          if hotspot_rssi < -130 or hotspot_rssi > -30 do
+          if hotspot_rssi < -130 or hotspot_rssi > 0 do
             {:error, "Invalid Uplink RSSI"}
           else
-            {:ok, "Valid Ingest Message"}
+            if hotspot_snr < -40 or hotspot_snr > 40 do
+              {:error, "Invalid Uplink SNR"}
+            else
+              {:ok, "Valid Ingest Message"}
+            end
           end
         end
       end
