271fd7a13b8c0d7357bebf6bfc7e61c32c9a4154
Kent Williams <k3nt00@gmail.com>
2022-01-26 18:40:24 -0800

    remove coverage endpoint

--- lib/mappers_web/router.ex
+++ lib/mappers_web/router.ex
@@ -32,12 +32,12 @@ defmodule MappersWeb.Router do
     get "/uplinks/hex/:h3_index", API.V1.UplinkController, :get_uplinks
   end
 
-  scope "/api/v1", MappersWeb do
-    pipe_through :api
-    pipe_through :allow_cors
+  # scope "/api/v1", MappersWeb do
+  #   pipe_through :api
+  #   pipe_through :allow_cors
 
-    get "/coverage/geo/:coords", API.V1.CoverageController, :get_coverage_from_geo
-  end
+  #   get "/coverage/geo/:coords", API.V1.CoverageController, :get_coverage_from_geo
+  # end
 
   # Enables LiveDashboard only for development
   #
