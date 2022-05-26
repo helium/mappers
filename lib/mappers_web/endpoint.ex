e39660cf678a4bea281d4df804f06d853723bef2
Kent Williams <k3nt00@gmail.com>
2022-05-26 11:09:07 -0700

    add < 55 sec timeout for heroku

--- lib/mappers_web/endpoint.ex
+++ lib/mappers_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule MappersWeb.Endpoint do
   ]
 
   socket "/socket", MappersWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
