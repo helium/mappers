916f3c343a58b38d24d50ee00e79a6f1b6fcdd6f
Kent Williams <k3nt00@gmail.com>
2023-07-03 15:23:17 -0700

    add db timeout to config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,8 @@ database_url =
 config :mappers, Mappers.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  timeout: 60_000
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
